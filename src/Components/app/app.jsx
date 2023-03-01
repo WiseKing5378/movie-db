6827eedeb0ea0f2fbf29c0fc61d4f9c2bfbc438e
WiseKing5378 <tonysigarev2000@gmail.com>
2023-03-01 16:49:45 +0500

    add loader and catch block for response

--- src/Components/app/app.jsx
+++ src/Components/app/app.jsx
@@ -1,9 +1,11 @@
 import { Component } from 'react';
+import { Spin, Alert } from 'antd';
 
 import CardList from '../card-list';
 import Header from '../header';
 import Footer from '../footer';
 import MovieAPI from '../movieAPI/movieAPI';
+import './app.css';
 
 export default class App extends Component {
   movieAPI = new MovieAPI();
@@ -12,35 +14,48 @@ export default class App extends Component {
     super();
     this.state = {
       movieData: [],
+      loading: true,
+      error: false,
     };
     this.setMovieData('hobbit');
   }
 
+  setError = () => {
+    this.setState({ error: true, loading: false });
+  };
+
   setMovieData = (name) => {
-    this.movieAPI.getData(name).then((result) => {
-      this.setState({
-        movieData: result.map((i) => {
-          return {
-            title: i.title,
-            overview: i.overview,
-            releaseDate: i.release_date,
-            posterPath: i.poster_path,
-            id: i.id,
-          };
-        }),
+    this.movieAPI
+      .getData(name)
+      .then((result) => {
+        this.setState({
+          movieData: result.map((i) => {
+            return {
+              title: i.title,
+              overview: i.overview,
+              releaseDate: i.release_date,
+              posterPath: i.poster_path,
+              id: i.id,
+            };
+          }),
+          loading: false,
+        });
+      })
+      .catch(() => {
+        this.setError();
       });
-    });
   };
 
   render() {
-    const { movieData } = this.state;
-    console.log(movieData);
-
+    const { movieData, loading, error } = this.state;
+    const loader = loading ? <Spin size="large" /> : null;
+    const content = !loading ? <CardList movieData={movieData} /> : null;
+    const err = error ? <Alert message="Movie not found" type="warning" showIcon /> : null;
     return (
       <section className="movieapp">
         <Header />
         <section className="main">
-          <CardList movieData={movieData} />
+          {loader} {content} {err}
         </section>
         <Footer />
       </section>
