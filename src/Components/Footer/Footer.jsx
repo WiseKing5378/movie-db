26e6099d3c6b5c084cf447588eaf7d9e3c6798e1
WiseKing5378 <tonysigarev2000@gmail.com>
2023-03-15 21:18:00 +0500

    update path

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable react/static-property-placement */
+import { Component } from 'react';
+import { Pagination } from 'antd';
+import PropTypes from 'prop-types';
+
+import './Footer.css';
+
+export default class Footer extends Component {
+  static defaultProps = {
+    getPage: () => {},
+    totalPages: 1,
+    newPage: 1,
+  };
+
+  static propTypes = {
+    getPage: PropTypes.func,
+    totalPages: PropTypes.number,
+    newPage: PropTypes.number,
+  };
+
+  render() {
+    const { getPage, totalPages, newPage } = this.props;
+
+    return (
+      <footer className="pagination">
+        <Pagination
+          showSizeChanger=""
+          onChange={getPage}
+          current={newPage}
+          total={totalPages}
+          defaultPageSize="20"
+          hideOnSinglePage
+        />
+      </footer>
+    );
+  }
+}
